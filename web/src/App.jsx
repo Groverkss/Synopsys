5b56cebf728dc4f8637da0edd1fa015844010842
v15hv4 <vishva2912@gmail.com>
2020-10-11 19:01:59 +0530

    Add placeholder pages and finalize skeleton

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Home from "./pages/Home";
+import Record from "./pages/Record";
+import NotFound from "./pages/NotFound";
+
+const App = () => {
+    return (
+        <>
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/records/:id" component={Record} />
+                    <Route exact path="/404" component={NotFound} />
+                    <Redirect to="/404" />
+                </Switch>
+            </Router>
+        </>
+    );
+};
 
 export default App;
